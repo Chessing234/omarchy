8a07b82e265f9264c53113ba9acb00f837a23dd5
Taksh <takshkothari09@gmail.com>
2026-09-12 22:15:48 +0530

    Strip CAP_DAC_READ_SEARCH from btop

--- test/shell.d/config-test.sh
+++ test/shell.d/config-test.sh
@@ -436,3 +436,9 @@ grep -Fq 'Exec = /usr/bin/omarchy-strip-gsr-kms-caps' \
   "$ROOT/default/libalpm/hooks/90-omarchy-strip-gsr-kms-caps.hook" ||
   fail "gsr-kms-server cap-strip hook must call omarchy-strip-gsr-kms-caps"
 pass "gsr-kms-server capability strip hook and command are present"
+
+[[ -f $ROOT/default/libalpm/hooks/90-omarchy-strip-btop-caps.hook ]] || fail "missing btop cap-strip pacman hook"
+[[ -x $ROOT/bin/omarchy-strip-btop-caps ]] || fail "missing omarchy-strip-btop-caps"
+grep -Fq 'Exec = /usr/bin/omarchy-strip-btop-caps' "$ROOT/default/libalpm/hooks/90-omarchy-strip-btop-caps.hook" ||
+  fail "btop cap-strip hook must call omarchy-strip-btop-caps"
+pass "btop capability strip hook and command are present"
